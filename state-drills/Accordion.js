2e1ab95343e3725c2f53046fa97983e2bc411bf7
Awesome Henry <henryebomah@gmail.com>
2021-01-14 10:34:02 -0800

    Initial

---
+++ state-drills/Accordion.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default class Accordion extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    state={
+        index: null
+    }
+    onClick(value){
+        this.setState({
+            index: value
+        })
+    }
+    render() {
+      return <ul>
+          {this.props.sections.map((section, index) =><li>
+              
+              {section.title}
+              <p>
+              {this.state.index==index?section.content:null}
+              </p>
+              <button onClick={e=>this.onClick(index)}>Click here</button>
+          </li>)}
+      </ul>
+    }
+}
+
